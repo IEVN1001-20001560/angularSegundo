d2b231c321baae3ca7f216fe8afe637f8f85466c
Ceidy Martinez <ceidy117@gmail.com>
2024-10-15 20:25:54 -0600

    Segundo commit RUTAS 15-10-24

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+export const routes: Routes=[
     {
         path:'auth',
         loadChildren:() => import ('./auth/freatures/auth.routes')
-    }
-];
+    }];
