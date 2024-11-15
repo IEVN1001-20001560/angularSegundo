171be408f5167c141f976dfb60811b38f8409490
Ceidy Martinez <ceidy117@gmail.com>
2024-11-14 21:03:50 -0600

    Commit final 14/11/24

--- src/app/utl/utl.routes.ts
+++ src/app/utl/utl.routes.ts
@@ -5,5 +5,17 @@ export default [
         path: 'listaalumnos',
         loadComponent:()=>import('./alumnos/alumnos.component')
 
+    }, 
+    {
+        path: 'agregar',
+        loadComponent:()=> import('./agregar/agregar.component')
+    }, 
+    {
+        path: 'eliminar/:matricula',
+        loadComponent:()=> import('./eliminar/eliminar.component')
+    }, 
+    {
+        path: 'editar/:matricula',
+        loadComponent:()=> import('./editar/editar.component')
     }
 ] as Routes
